450c38e775518cd0fa07469144b3479035afbfd8
Than Ngoc Quang <97534002+RedEyeSH@users.noreply.github.com>
2025-03-09 15:02:29 +0200

    Update MariaDbJpaConnection.java

--- 7.3/src/main/java/org/example/currencyconverterjpa/Datasource/MariaDbJpaConnection.java
+++ 7.3/src/main/java/org/example/currencyconverterjpa/Datasource/MariaDbJpaConnection.java
@@ -1,33 +1,3 @@
-//package org.example.currencyconverterjpa.Datasource;
-//
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.SQLException;
-//
-//public class MariaDbJpaConnection {
-//    private static final String URL = "jdbc:mariadb://localhost:3307/currencydb";
-//    private static final String USER = "root";
-//    private static final String PASSWORD = "Metropolia05";
-//
-//    private static Connection connection;
-//
-//    private MariaDbJpaConnection() {}
-//
-//    public static Connection getConnection() throws SQLException {
-//        if (connection == null || connection.isClosed()) {
-//            try {
-//                connection = DriverManager.getConnection(URL, USER, PASSWORD);
-//                System.out.println("Database connected successfully.");
-//            } catch (SQLException e) {
-//                System.err.println("Failed to connect to database.");
-//                e.printStackTrace();
-//                throw e;
-//            }
-//        }
-//        return connection;
-//    }
-//}
-
 package org.example.currencyconverterjpa.Datasource;
 
 import jakarta.persistence.*;
