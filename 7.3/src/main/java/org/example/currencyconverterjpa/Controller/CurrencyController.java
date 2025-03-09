971dcda5be9e62651c52a23b66fa6c4661d46457
Than Ngoc Quang <97534002+RedEyeSH@users.noreply.github.com>
2025-03-09 15:00:50 +0200

    Update CurrencyController.java

--- 7.3/src/main/java/org/example/currencyconverterjpa/Controller/CurrencyController.java
+++ 7.3/src/main/java/org/example/currencyconverterjpa/Controller/CurrencyController.java
@@ -1,30 +1,3 @@
-//package org.example.currencyconverterjpa.Controller;
-//
-//import org.example.currencyconverterjpa.Dao.CurrencyDao;
-//
-//public class CurrencyController {
-//    private final CurrencyDao currencyDao;
-//
-//    public CurrencyController() {
-//        this.currencyDao = new CurrencyDao();
-//    }
-//
-//    public String[] getAvailableCurrencies() {
-//        return currencyDao.getAllCurrencyAbbreviations().toArray(new String[0]);
-//    }
-//
-//    public double convertAmount(double amount, String fromCurrency, String toCurrency) {
-//        double fromRate = currencyDao.getExchangeRate(fromCurrency);
-//        double toRate = currencyDao.getExchangeRate(toCurrency);
-//
-//        if (fromRate == 0 || toRate == 0) {
-//            throw new IllegalArgumentException("Invalid currency conversion rates.");
-//        }
-//
-//        return (amount / fromRate) * toRate;
-//    }
-//}
-
 package org.example.currencyconverterjpa.Controller;
 
 import org.example.currencyconverterjpa.Dao.CurrencyDao;
